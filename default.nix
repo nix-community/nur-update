2d273032ab481b2419065a7c993901675010a95d
Jörg Thalheim <joerg@thalheim.io>
2020-11-30 09:25:33 +0100

    lint + format + type check

--- default.nix
+++ default.nix
@@ -1,8 +1,19 @@
 with import <nixpkgs> {};
 python3Packages.buildPythonApplication {
-  name = "env";
+  name = "nur-update";
   src = ./.;
   propagatedBuildInputs = [
     python3Packages.flask
   ];
+  checkInputs = [
+    mypy python3.pkgs.black python3.pkgs.flake8 glibcLocales
+  ];
+  checkPhase = ''
+    echo -e "\x1b[32m## run black\x1b[0m"
+    LC_ALL=en_US.utf-8 black --check .
+    echo -e "\x1b[32m## run flake8\x1b[0m"
+    flake8 nur_update
+    echo -e "\x1b[32m## run mypy\x1b[0m"
+    mypy --strict nur_update
+  '';
 }
