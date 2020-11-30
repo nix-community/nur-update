62f1cc631a7a040e20f482d849d6a219662b3f3a
Jörg Thalheim <joerg@thalheim.io>
2020-11-30 11:38:37 +0100

    print version of black/flake8/mypy

--- default.nix
+++ default.nix
@@ -10,10 +10,13 @@ python3Packages.buildPythonApplication {
   ];
   checkPhase = ''
     echo -e "\x1b[32m## run black\x1b[0m"
+    black --version
     LC_ALL=en_US.utf-8 black --check .
     echo -e "\x1b[32m## run flake8\x1b[0m"
+    flake8 --version
     flake8 nur_update
     echo -e "\x1b[32m## run mypy\x1b[0m"
+    mypy --version
     mypy --strict nur_update
   '';
 }
